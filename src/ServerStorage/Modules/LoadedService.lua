d7f48a63b9432324b9a05085380ec6e1680255de
Tazmondo <fondcoolman@outlook.com>
2023-12-15 00:15:16 +0000

    add loadedservice

---
+++ src/ServerStorage/Modules/LoadedService.lua
@@ -0,0 +1,33 @@
+local LoadedService = {}
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Future = require(ReplicatedStorage.Packages.Future)
+local Loaded = require(ReplicatedStorage.Events.Loaded):Server()
+
+local loadedAttribute = "LoadedService_Loaded"
+
+function HandleLoaded(player: Player)
+	player:SetAttribute(loadedAttribute, true)
+end
+
+function LoadedService:GetLoadedFuture(player: Player)
+	return Future.new(function(player: Player)
+		while player.Parent ~= nil and not player:GetAttribute(loadedAttribute) do
+			task.wait()
+		end
+
+		return player.Parent ~= nil
+	end, player)
+end
+
+function LoadedService:HasPlayerLoaded(player: Player)
+	return player:GetAttribute(loadedAttribute) == true
+end
+
+function LoadedService:Initialize()
+	Loaded:On(HandleLoaded)
+end
+
+LoadedService:Initialize()
+
+return LoadedService
