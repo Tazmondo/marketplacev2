452e231142a9c82c172dcd63e50623f561f118f4
Tazmondo <fondcoolman@outlook.com>
2023-12-20 19:53:38 +0000

    re-enable player list

--- src/StarterPlayer/StarterPlayerScripts/coregui.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/coregui.client.lua
@@ -1,4 +1,4 @@
 -- Need to disable these three to stop the top right menu from appearing
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
+-- game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
+-- game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
