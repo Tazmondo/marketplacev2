4bcd163ca0835b7a51ffd42fe5a43cba367df766
Tazmondo <fondcoolman@outlook.com>
2023-12-21 22:50:31 +0000

    create rate limiting system for random feed fetching

--- src/ReplicatedStorage/Modules/Shared/Util.lua
+++ src/ReplicatedStorage/Modules/Shared/Util.lua
@@ -44,4 +44,41 @@ function Util.PrettyPrint(table: { [any]: any })
 	print(out)
 end
 
+-- Note that this should only be used in non-greedy situations
+-- I.e. where this being blocked is not a problem - it is a constant process
+-- I had extending the random feed list in mind when writing this.
+function Util.CreateRateDelay(interval: number)
+	local lastCalled = 0
+	local resumptionQueue = {}
+
+	local function popResume()
+		lastCalled = os.clock()
+
+		local thread = assert(table.remove(resumptionQueue, 1))
+		if #resumptionQueue > 0 then
+			task.delay(interval, popResume)
+		end
+
+		coroutine.resume(thread)
+	end
+
+	local function rateDelay()
+		if (os.clock() - lastCalled) >= interval then
+			lastCalled = os.clock()
+			return
+		end
+		if #resumptionQueue == 0 then
+			task.delay(interval - (os.clock() - lastCalled), popResume)
+		end
+
+		table.insert(resumptionQueue, coroutine.running())
+		if #resumptionQueue >= 10 then
+			warn(debug.traceback("Rate delay may be getting overloaded!"))
+		end
+		coroutine.yield()
+	end
+
+	return rateDelay
+end
+
 return Util
--- src/ServerStorage/Modules/Feed/RandomFeed.lua
+++ src/ServerStorage/Modules/Feed/RandomFeed.lua
@@ -6,13 +6,18 @@ local Data = require(ReplicatedStorage.Modules.Shared.Data)
 local DataService = require(ServerStorage.Modules.DataService)
 local RandomPlayerService = require(ServerStorage.Modules.RandomPlayerService)
 local Types = require(ReplicatedStorage.Modules.Shared.Types)
+local Util = require(ReplicatedStorage.Modules.Shared.Util)
 local Future = require(ReplicatedStorage.Packages.Future)
 
 local FEEDSEGMENTLENGTH = 10
+local FEEDRATELIMIT = 5
 
 local usedShowcases: { [string]: true? } = {}
-local cachedRandomFeed: { Types.Showcase }? = nil
+local cachedFeed: { Types.Showcase } = {}
 local random = Random.new()
+
+local feedFuture: Future.Future<{ Types.Showcase }>? = nil
+local rateLimit = Util.CreateRateDelay(FEEDRATELIMIT)
 
 function IsShowcaseValid(showcase: Data.Showcase): boolean
 	-- We don't want empty showcases clogging up the feed
@@ -21,6 +26,9 @@ end
 
 function GenerateFeedSegment()
 	return Future.new(function()
+		-- If this is being called too much then wait
+		rateLimit()
+
 		local feed = {}
 		local ignoreSet = {}
 
@@ -60,17 +68,41 @@ function GenerateFeedSegment()
 	end)
 end
 
-function RandomFeed.GetFeed()
-	return Future.new(function()
-		if cachedRandomFeed then
-			return cachedRandomFeed
+--- Attempts to get a random feed of up to the desired length.
+--- May return less than the desired length if there aren't enough valid feeds.
+--- Also ensures there is only one feed fetch operation ongoing at a time
+
+function RandomFeed.GetFeed(desiredLength: number?)
+	local future = Future.new(function()
+		if not desiredLength or desiredLength <= #cachedFeed then
+			return cachedFeed
 		end
 
-		local newFeed = GenerateFeedSegment():Await()
-		cachedRandomFeed = newFeed
+		if feedFuture then
+			local lastFeed = feedFuture:Await()
+			if #lastFeed >= desiredLength then
+				return lastFeed
+			end
+		end
 
-		return newFeed
+		while #cachedFeed <= desiredLength do
+			local newFeed = GenerateFeedSegment():Await()
+			if #newFeed == 0 then
+				-- No more showcases can be fetched.
+				return cachedFeed
+			end
+
+			for i, showcase in newFeed do
+				table.insert(cachedFeed, showcase)
+			end
+		end
+
+		return cachedFeed
 	end)
+
+	feedFuture = future
+
+	return future
 end
 
 task.spawn(function()
