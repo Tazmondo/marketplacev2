351cde082a8f71f74a9f685d91daccbd6219ba3e
Tazmondo <fondcoolman@outlook.com>
2024-01-07 00:49:11 +0000

    disable reset button

--- src/StarterPlayer/StarterPlayerScripts/coregui.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/coregui.client.lua
@@ -1,4 +1,17 @@
+local StarterGui = game:GetService("StarterGui")
 -- Need to disable these three to stop the top right menu from appearing
--- game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
--- game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+-- StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
+-- StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+
+-- Need to wait for client scripts to register this, so it may error at first.
+local successful = false
+local start = os.clock()
+repeat
+	successful = pcall(StarterGui.SetCore, StarterGui, "ResetButtonCallback", false)
+	if os.clock() - start > 5 then
+		warn("Took too long to disable reset!")
+		return
+	end
+	task.wait()
+until successful == true
