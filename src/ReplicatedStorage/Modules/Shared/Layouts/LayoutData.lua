02b71226e469008d1e7611672debe9f87ac8c176
Tazmondo <fondcoolman@outlook.com>
2024-01-16 20:06:30 +0000

    update layouts

--- src/ReplicatedStorage/Modules/Shared/Layouts/LayoutData.lua
+++ src/ReplicatedStorage/Modules/Shared/Layouts/LayoutData.lua
@@ -1,15 +1,16 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TableUtil = require(ReplicatedStorage.Packages.TableUtil)
 
-export type LayoutId = "Shop 1" | "Shop 2" | "Shop 3" | "Shop 4" | "Shop 5" | "Shop 6"
+export type LayoutId = "Shop 1" | "Shop 2" | "Shop 3" | "Shop 4" | "Shop 5" | "Shop 6" | "Shop 7"
 
 local layoutData: { [LayoutId]: number } = {
 	["Shop 1"] = 15688473519,
 	["Shop 2"] = 15688473638,
 	["Shop 3"] = 15688473772,
 	["Shop 4"] = 15693431898,
-	["Shop 5"] = 15693431898,
-	["Shop 6"] = 15693431898,
+	["Shop 5"] = 15998005134,
+	["Shop 6"] = 15998007352,
+	["Shop 7"] = 16000178420,
 }
 
 TableUtil.Lock(layoutData)
